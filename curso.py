5c35857160f28e2f54e0933b3c67bf338246bd04
Vanikoro <Vanikoro@email.com>
2021-07-31 02:00:08 -0500

    Reto 2: Índice de masa corporal

--- curso.py
+++ curso.py
@@ -5,3 +5,10 @@ def area_triangulo (x:float, y:float, z: float) -> float:
     x = s*(s-x)*(s-y)*(s-z)
     area = round(math.sqrt(x),1)
     return area
+
+def calcular_BMI (x:float, y:float) -> float:
+    b = x*0.45
+    c = y*0.025
+    a = b/c**2
+    BMI = round(a,2)
+    return BMI
