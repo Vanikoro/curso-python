2316b838c2e68a182f0afe735afd8ca3d285f352
Vanikoro <Vanikoro@email.com>
2021-08-01 23:51:53 -0500

    Reto 1: Modularidad

---
+++ semana2.py
@@ -0,0 +1,15 @@
+def es_divisible (n:int,d:int) -> int:
+    if d != 0:
+        if n%(2*d) == 0:
+            resultado = 2
+        elif n%d == 0 and n%(2*d) !=0:
+            resultado = 1
+        else:
+            resultado = 0
+    else:
+         resultado = 0
+    return str(resultado)      
+        
+    
+    
+    
