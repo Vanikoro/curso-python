3e6716e1e21eabeeb66c9c6557436c410aba6059
Vanikoro <Vanikoro@email.com>
2021-08-02 17:14:20 -0500

    Reto 2: Regalo de santa

--- semana2.py
+++ semana2.py
@@ -1,3 +1,4 @@
+"""
 def es_divisible (n:int,d:int) -> int:
     if d != 0:
         if n%(2*d) == 0:
@@ -9,7 +10,33 @@ def es_divisible (n:int,d:int) -> int:
     else:
          resultado = 0
     return resultado    
+  """
+def es_palindromo (id:int) -> int:
+    u = id % 10
+    id //= 10
+    d = id % 10
+    id //= 10
+    c = id % 10
+    inv = (u * 100) + (d * 10) + c
+    return inv
+  
+def clasificar_regalo (id:int) -> str:
+    if id > 100 and id < 999:
+        numero = es_palindromo(id)
+        if numero == id:
+            if id % 2 != 0:
+                res = "girl"
+            else:
+                res = "boy"
+        else:
+            if id % 2 != 0:
+                res = "woman"
+            else:
+                res = "man"
+    return res
         
+                
+
     
     
     
